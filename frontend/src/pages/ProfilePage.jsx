8ff801e5cb7fe9d927358d333fc2db164488ebc9
xehrf <ococlydasdla@gmail.com>
2026-04-26 21:45:12 +0500

    fix xnjnj

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -4,7 +4,7 @@ import { LinkButton } from "../components/ui/Button.jsx";
 import { Card } from "../components/ui/Card.jsx";
 import { apiFetch, resolveAssetUrl } from "../api/client";
 import { jsPDF } from "jspdf";
-import html2canvas from "html2canvas";
+import * as html2canvasModule from "html2canvas";
 
 async function generateResumePDF(profile, completedTasks, skillChips) {
 
@@ -170,8 +170,8 @@ async function generateResumePDF(profile, completedTasks, skillChips) {
 
   try {
     await new Promise(r => setTimeout(r, 300));
-  const h2c = html2canvas.default || html2canvas;
-  const canvas = await h2c(container.firstElementChild, {
+    const h2c = html2canvasModule.default ?? html2canvasModule;
+    const canvas = await h2c(container.firstElementChild, {
       scale: 2,
       useCORS: true,
       allowTaint: true,
